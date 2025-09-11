cb17cf0be3f344e9876f133b7f26009a723eb326
SagiBeno <benosagi321@gmail.com>
2025-09-11 13:27:55 +0200

    File-ok létrehozása, diak.png beillesztése

---
+++ src/main/java/com/example/diakok/DiakokApplication.java
@@ -0,0 +1,24 @@
+package com.example.diakok;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DiakokApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(DiakokApplication.class.getResource("diakok-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Diákok");
+        //stage.setIconified(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
